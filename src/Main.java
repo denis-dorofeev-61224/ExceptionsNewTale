17096b8765a9bf78ffe5a2c2f05d56001d69d496
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2024-11-03 15:30:59 +0300

    Branch of developer-2 The main code.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello JavaDeveloper!");
+        System.out.println("====================");
+        System.out.println("THIS IS BRANCH OF DEVELOPER-2");
+        System.out.println("=====================");
+        String login = "java_skypro_g";
+        String password = "D_1hWiKjjP_9";
+        String confirmPassword = "D_1hWiKjjP_9";
+        try {
+            UserValidator.validate(login, password, confirmPassword);
+            System.out.println("Login and password are valid!");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.err.println("Validation error: " + e.getMessage());
+        }
     }
 }
