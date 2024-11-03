8d3da89bc42d693192bdca1d0a447b5a399e6852
denis-dorofeev61224 <denis_dorofeev_1977@mail.ru>
2024-11-03 14:27:52 +0300

    Branch of developer-1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello JavaDeveloper!");
+        System.out.println("********************");
+        System.out.println("THIS IS BRANCH OF Developer-1");
     }
 }
